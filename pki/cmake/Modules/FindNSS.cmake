a27fda85a04b0b34e0ee8aff8cef99790c8c0ec0
James S White <james.white@github.com>
2013-12-30 15:40:30 -0600

    hard-code that first path

--- pki/cmake/Modules/FindNSS.cmake
+++ pki/cmake/Modules/FindNSS.cmake
@@ -28,6 +28,7 @@ else (NSS_LIBRARIES AND NSS_INCLUDE_DIRS)
       nss.h
     PATHS
       ${_NSS_INCLUDEDIR}
+      /usr/include/nss
       /usr/include
       /usr/local/include
       /opt/local/include
